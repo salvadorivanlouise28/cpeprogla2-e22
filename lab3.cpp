f2742d53b54b6440b4821b71bcb953661fc03cc9
CHRISTOPHER IVAN LOUISE SALVADOR <CMSALVADOR@fit.edu.ph>
2016-09-10 11:14:48 +0800

    Initial lab3

---
+++ lab3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int mystrCmp*yo(char*str, char*str2){
+	
+	return strcmp(str1.str2);
+
+		 
+	}
+	
+}
+int main ()
+{
+	char str1[100]="Hello";
+	char str2[100]="hello";
+	
+	cout << myStrCmp(str1, str2);
+}
