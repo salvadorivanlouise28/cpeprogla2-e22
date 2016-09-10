8a3abd95bd364188997d7e0a59c24acbc260e0bb
CHRISTOPHER IVAN LOUISE SALVADOR <CMSALVADOR@fit.edu.ph>
2016-09-10 11:42:11 +0800

    Adds custom strcpy

--- lab3.cpp
+++ lab3.cpp
@@ -2,18 +2,33 @@
 #include <cstring>
 using namespace std;
 
-int mystrCmp*yo(char*str, char*str2){
+char* mystrCmp*yo(char*str, char*str2){
 	
-	return strcmp(str1.str2);
+	switch(strcmp(str1.str2)){
+	 case -1:
+	      cout<<"negative";
+	      break;
+     case 0:
+          cout<<"equal";
+          break;
+     case 1:
+     	  cout<<"positive";
+     	  break;
+	}
 
 		 
 	}
 	
 }
 int main ()
+void myStrcpy(char* str1, char* str2){
+	strcpy(str1, str2);
+}
 {
 	char str1[100]="Hello";
 	char str2[100]="hello";
 	
-	cout << myStrCmp(str1, str2);
+	myStrCmp(str1, str2);
+	myStrCpy(str1, str2);
+    cout << myStrCmp(str1, str2);
 }
